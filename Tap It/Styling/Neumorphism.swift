b1f3513dbf123033d10e248e0691d2f748244a9b
Nikita Mounier <nikita.mounier@gmail.com>
2021-01-29 21:30:38 +0100

    Updated neumorphic colours.

--- Tap It/Styling/Neumorphism.swift
+++ Tap It/Styling/Neumorphism.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 enum Neumorphic {
-    static let defaultMainColor = Color(red: 0.925, green: 0.941, blue: 0.953, opacity: 1.000)
+    static let defaultMainColor = Color(red: 238 / 255, green: 240 / 255, blue: 243 / 255, opacity: 1.000)
     static let defaultSecondaryColor = Color(red: 0.482, green: 0.502, blue: 0.549, opacity: 1.000)
+    
+    static let defaultBackgroundColor = Color(red: 229 / 255, green: 229 / 255, blue: 229 / 255)
+    
     static let defaultLightShadowSolidColor = Color(red: 1.000, green: 1.000, blue: 1.000, opacity: 1.000)
     static let defaultDarkShadowSolidColor = Color(red: 0.820, green: 0.851, blue: 0.902, opacity: 1.000)
+    
     static let defaultLightShadowColor = Color(red: 1.000, green: 1.000, blue: 1.000, opacity: 0.500)
     static let defaultDarkShadowColor = Color(red: 0.66, green: 0.72, blue: 0.84, opacity: 0.55)
     
     static var mainColor: Color {
+        return defaultMainColor
+    }
+    
+    static var backgroundColor: Color {
         return defaultMainColor
     }
     
