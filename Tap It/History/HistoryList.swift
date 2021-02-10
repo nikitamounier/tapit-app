dc35350a36424551331d0c6b75b4a0337915497b
Nikita Mounier <nikita.mounier@gmail.com>
2021-02-10 17:19:38 +0100

    Decided not to put a search bar in each list category - so won't be needing ScrollViewReader to automatically scroll past the aforementioned search bar.

--- Tap It/History/HistoryList.swift
+++ Tap It/History/HistoryList.swift
@@ -9,24 +9,15 @@ import SwiftUI
 
 struct HistoryList: View {
     let category: History.Category
-    @ScaledMetric private var dynamicTextFieldHeight: CGFloat = 40
     
     var body: some View {
         ScrollView {
-            ScrollViewReader { proxy in
-                VStack(spacing: 0) {
-                    LazyVStack(spacing: 15) {
-                        ForEach(0..<100) { item in
-                            HistoryCell(profilePicture: UIImage(systemName: "person.crop.square.fill") ?? .init(), name: "Nikita Mounier", socials: [], date: "27/01")
-                        }
-                    }
-                    .id(0)
-                    .padding(.top, 15)
-                }
-                .onAppear {
-                    proxy.scrollTo(0, anchor: .top) // skip TextField, should only show up if user scroll up
+            LazyVStack(spacing: 15) {
+                ForEach(0..<100) { item in
+                    HistoryCell(profilePicture: UIImage(systemName: "person.crop.square.fill") ?? .init(), name: "Nikita Mounier", socials: [], date: "27/01")
                 }
             }
+            .padding(.top, 15)
         }
     }
 }
