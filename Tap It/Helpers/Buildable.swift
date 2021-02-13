908769d344c4e40ad39e4399d0a475a8d8a7d057
Nikita Mounier <nikita.mounier@gmail.com>
2021-02-13 17:15:48 +0100

    Corrected spelling mistake.

--- Tap It/Helpers/Buildable.swift
+++ Tap It/Helpers/Buildable.swift
@@ -12,7 +12,7 @@ extension Buildable {
     /// Helper function to mutate the property of an instance using Builder pattern
     /// - Parameters:
     ///   - keyPath: `WriteableKeyPath`to the instance property being modified
-    ///   - value: The new vlaue of the instance property
+    ///   - value: The new value of the instance property
     func mutating<T>(keyPath: WritableKeyPath<Self, T>, value: T) -> Self {
         var newSelf = self
         newSelf[keyPath: keyPath] = value
