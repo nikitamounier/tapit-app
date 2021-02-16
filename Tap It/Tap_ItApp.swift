55c1803d2637f728acaafb683ae9f0fbd54502ec
Nikita Mounier <nikita.mounier@gmail.com>
2021-02-17 00:47:55 +0100

    Added Recombine store iwhich will be injected into the environment.

--- Tap It/Tap_ItApp.swift
+++ Tap It/Tap_ItApp.swift
@@ -9,12 +9,26 @@ import SwiftUI
 
 @main
 struct Tap_ItApp: App {
-    let persistenceController = PersistenceController.shared
+    let store = Redux.store
+    
+    @Environment(\.scenePhase) private var scenePhase
 
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(store)
+        }
+        .onChange(of: scenePhase) { phase in
+            switch phase {
+            case .background: // where would dispatch 'background' raw action, aka save state to FileManager
+                break
+            case .inactive:
+                break
+            case .active: // where would dispatch 'active' raw action, aka load state from disk if not already loaded
+                break
+            @unknown default:
+                break
+            }
         }
     }
 }
