9253e1a1d39cfcebe4db32c68ffdc2afc9e92d2e
Nikita Mounier <nikita.mounier@gmail.com>
2021-02-17 00:48:40 +0100

    Injecting Recombine store instead of managedObject context in ContentView previews.

--- Tap It/ContentView.swift
+++ Tap It/ContentView.swift
@@ -17,6 +17,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView().environmentObject(Redux.store)
     }
 }
