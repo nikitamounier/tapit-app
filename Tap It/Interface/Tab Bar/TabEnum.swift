d35183c70c75a7cf4362758dcef3dfdb8ae5955d
Nikita Mounier <nikita.mounier@gmail.com>
2021-02-17 02:12:27 +0100

    Isolated tab enum.

---
+++ Tap It/Interface/Tab Bar/TabEnum.swift
@@ -0,0 +1,12 @@
+//
+//  TabBarViewModel.swift
+//  Tap It
+//
+//  Created by Nikita Mounier on 04/02/2021.
+//
+
+enum Tab: String, Codable {
+    case history = "rectangle.stack.person.crop"
+    case tap = "" // since using custom image in the future
+    case profile = "person.crop.circle"
+}
