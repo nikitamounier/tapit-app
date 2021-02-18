b6c277e199b4c775ecbf4a2433d8da20f5ea12a7
Nikita Mounier <nikita.mounier@gmail.com>
2021-02-18 20:33:10 +0100

    Refactored reducers and added persistence actions.

--- Tap It/Redux/Reducer.swift
+++ Tap It/Redux/Reducer.swift
@@ -11,16 +11,29 @@ extension Redux {
     enum Reducer {
         static let main = MutatingReducer<State, Action.Refined> { state, action in
             switch action {
+            case let .setState(appState):
+                state = appState
+                
             case let .tabAction(modification):
-                state.tabSelection = tabReducer(state: state.tabSelection, action: modification)
+                state.tabSelection = tabReducer(state: state.tabSelection, action: modification) // pure reducer, because lightweight type
+                
+            case let .tappedProfilesAction(modification):
+                tappedProfilesReducer(state: &state.tappedProfiles, action: modification) // inout reducer - wouldn't want to replace dictionary every time
+            
+            case .none:
+                break
             }
         }
         
-        private static let tabReducer = PureReducer<State.TabSelection, Action.Refined.TabAction> { _, tabAction in
+        private static let tabReducer = PureReducer<State.TabState, Action.Refined.TabAction> { _, tabAction in
             switch tabAction {
             case let .setTab(to: tab):
-                return State.TabSelection(currentTab: tab)
+                return State.TabState(currentTab: tab)
             }
+        }
+        
+        private static let tappedProfilesReducer = MutatingReducer<State.TappedProfilesState, Action.Refined.TappedProfilesAction> { state, profilesAction in
+            
         }
     }
 }
