75cfc3672bbe0f05557d72067dbae534ebcddfea
Nikita Mounier <nikita.mounier@gmail.com>
2021-02-18 20:29:42 +0100

    Added Combine implementation of FileManager - used in the middleware

---
+++ Tap It/Helpers/FileManager+Combine.swift
@@ -0,0 +1,46 @@
+//
+//  FileManager+Combine.swift
+//  Tap It
+//
+//  Created by Nikita Mounier on 18/02/2021.
+//
+
+import Combine
+import SwiftUI
+
+extension FileManager {
+    func load(from name: String, in directory: SearchPathDirectory) -> AnyPublisher<Data, Error> {
+        return Deferred {
+            Future { promise in
+                do {
+                    let documentsURL = try self.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+                    let fileURL = documentsURL.appendingPathComponent(name)
+                    promise(.success(try Data(contentsOf: fileURL)))
+                } catch {
+                    print("Error loading from disk: \(error)") // temporary
+                    promise(.failure(error))
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func save(data: Data, to name: String, in directory: SearchPathDirectory) -> AnyPublisher<Void, Error> {
+        return Deferred {
+            Future { promise in
+                do {
+                    let documentsURL = try self.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+                    let fileURL = documentsURL.appendingPathComponent(name)
+                    
+                    try data.write(to: fileURL)
+                    promise(.success(()))
+                } catch {
+                    print("Error reading to disk: \(error)") // temporary
+                    promise(.failure(error))
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+}
+
