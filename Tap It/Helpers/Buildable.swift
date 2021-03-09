14c29801ad1f42da1142d439e2dfc4f3e6f09f35
Nikita Mounier <nikita.mounier@gmail.com>
2021-03-09 19:22:00 +0000

    Added reference type overload and significant documentation.

--- Tap It/Helpers/Buildable.swift
+++ Tap It/Helpers/Buildable.swift
@@ -13,8 +13,40 @@ extension Buildable {
     /// - Parameters:
     ///   - keyPath: `WriteableKeyPath` to the property which shall be modified
     ///   - value: The value which will be set to the property
+    ///z
+    /// Say you have a person type, like so:
     ///
-    /// This function is used for types with value semantics.
+    ///     struct Person {
+    ///         var name: String?
+    ///         var age: Int?
+    ///         var city: String?
+    ///         var country: String?
+    ///         var planet: String?
+    ///     }
+    ///
+    /// You declare the builder functions:
+    ///
+    ///     extension Person: Buildable {
+    ///         func called(_ name: String) -> Self {
+    ///             build(keyPath: \.name, value: name)
+    ///         }
+    ///
+    ///         func aged(_ age: Int) -> Self {
+    ///             build(keyPath: \.age, value: age)
+    ///         }
+    ///
+    ///         func living(in city: String, country: String, planet: String) -> Self {
+    ///             build(keyPath: \.city, value: city)
+    ///             .build(keyPath: \.country, value: country)
+    ///             .build(keyPath: \.planet, value: planet) // highly composable
+    ///         }
+    ///
+    /// And at declaration, build your type:
+    ///
+    ///     let person = Person()
+    ///         .called("Chris")
+    ///         .aged(25)
+    ///         .living(in: "San Francisco", country: "United States", planet: "Earth")
     func build<Value>(keyPath: WritableKeyPath<Self, Value>, value: Value) -> Self {
         var newSelf = self
         newSelf[keyPath: keyPath] = value
@@ -26,7 +58,8 @@ extension Buildable {
     ///   - keyPath: `ReferenceWriteableKeyPath` to the property which shall be modified
     ///   - value: The value which will be set to the property
     ///
-    /// This function is used for types with reference semantics.
+    /// This function is used for reference types.
+    /// For additional documentation, refer to the other `build` function made for value types.
     func build<Value>(keyPath: ReferenceWritableKeyPath<Self, Value>, value: Value) -> Self {
         let newSelf = self
         newSelf[keyPath: keyPath] = value
