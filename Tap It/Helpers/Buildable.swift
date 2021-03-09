c74b5d9f3dac0bebf7e9725bad70cf5958e65dcd
Nikita Mounier <nikita.mounier@gmail.com>
2021-03-09 18:53:46 +0000

    Added new reference type overload and added more documentation.

--- Tap It/Helpers/Buildable.swift
+++ Tap It/Helpers/Buildable.swift
@@ -9,12 +9,26 @@
 protocol Buildable {}
 
 extension Buildable {
-    /// Helper function to mutate the property of an instance using Builder pattern
+    /// Returns a new instance whose property defined by the keypath is set to `value`.
     /// - Parameters:
-    ///   - keyPath: `WriteableKeyPath`to the instance property being modified
-    ///   - value: The new value of the instance property
-    func mutating<T>(keyPath: WritableKeyPath<Self, T>, value: T) -> Self {
+    ///   - keyPath: `WriteableKeyPath` to the property which shall be modified
+    ///   - value: The value which will be set to the property
+    ///
+    /// This function is used for types with value semantics.
+    func build<Value>(keyPath: WritableKeyPath<Self, Value>, value: Value) -> Self {
         var newSelf = self
+        newSelf[keyPath: keyPath] = value
+        return newSelf
+    }
+    
+    /// Returns a new instance whose property defined by the keypath is set to `value`.
+    /// - Parameters:
+    ///   - keyPath: `ReferenceWriteableKeyPath` to the property which shall be modified
+    ///   - value: The value which will be set to the property
+    ///
+    /// This function is used for types with reference semantics.
+    func build<Value>(keyPath: ReferenceWritableKeyPath<Self, Value>, value: Value) -> Self {
+        let newSelf = self
         newSelf[keyPath: keyPath] = value
         return newSelf
     }
