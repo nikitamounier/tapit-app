28a4d7c72e503bcec13aef35fdb16c24caa43206
Nikita Mounier <nikita.mounier@gmail.com>
2021-04-07 23:22:26 +0100

    Refactored ProfilePreview.

--- Tap It/Interface/History Screen/HistoryCell.swift
+++ Tap It/Interface/History Screen/HistoryCell.swift
@@ -15,11 +15,55 @@ struct HistoryCell: View {
     
     @ScaledMetric private var dynamicHeight: CGFloat = 105
     
+    let tempSocials = Image.SocialIcon.allCases
+        .shuffled()
+        .prefix(Int.random(in: 2...4))
+    
+    var profileIcon: some View {
+        Image(uiImage: profilePicture)
+            .resizable()
+            .clipShape(Circle())
+    }
+    
+    var topContent: some View {
+        HStack(alignment: .bottom, spacing: 0) {
+            Text(name)
+                .font(.headline)
+            Spacer()
+            (Text(Image(systemName: "calendar")) + Text(" \(date)"))
+                .foregroundColor(.gray)
+                .padding(.trailing)
+                .font(.footnote)
+        }
+        .lineLimit(1)
+    }
+    
+    func bottomContent(geo: GeometryProxy) -> some View {
+        HStack(alignment: .center) {
+            HStack {
+                ForEach(tempSocials, id: \.rawValue) { social in
+                    Button(action: {}) {
+                        Image(social: social)
+                            .resizable()
+                            .scaledToFit()
+                            .cornerRadius(10)
+                    }
+                }
+                Spacer()
+            }
+            LinearGradient(tapGradient: .leftToRight)
+                .mask(Image(systemName: "ellipsis").font(.title))
+                .frame(width: geo.size.width / 10, height: geo.size.height / 10)
+                .padding(.trailing)
+        }
+        .frame(height: geo.size.height / 3)
+    }
+    
     var body: some View {
         RoundedRectangle(cornerRadius: 25, style: .continuous)
             .fill(Neumorphic.mainColor)
             .neumorphicOuter()
-            .overlay(ProfilePreview(profilePicture: profilePicture, name: name, socials: socials, date: date))
+            .overlay(ProfilePreview(profileIcon: profileIcon, topContent: topContent, bottomContent: bottomContent))
             .frame(height: dynamicHeight)
             .padding(.horizontal)
     }
