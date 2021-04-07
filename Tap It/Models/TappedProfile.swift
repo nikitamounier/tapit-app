b12b4d78322a5a565d7abfad4161e84353fa95d3
Nikita Mounier <nikita.mounier@gmail.com>
2021-04-07 23:30:27 +0100

    Created basic tapped profile model, mostly for prototyping purposes.

---
+++ Tap It/Models/TappedProfile.swift
@@ -0,0 +1,13 @@
+//
+//  TappedProfile.swift
+//  Tap It
+//
+//  Created by Nikita Mounier on 23/02/2021.
+//
+
+import SwiftUI
+
+struct TappedProfile: Codable, Identifiable, Equatable {
+    var id = UUID()
+    var dateAdded = Date()
+}
