2c2a05097a48653d4731bf05706025b424dff796
Nikita Mounier <nikita.mounier@gmail.com>
2021-04-07 23:15:51 +0100

    Added more state and experimental Sourcery state tree.

--- Tap It/Redux/State.swift
+++ Tap It/Redux/State.swift
@@ -12,12 +12,16 @@ enum Redux {}
 protocol AutoTree {}
 
 extension Redux {
-    struct State: Codable, Equatable, AutoTree {
+    struct State: Codable, Equatable {
         var tabSelection: TabState = .init()
         var tappedProfiles: TappedProfilesState = .init()
         
         struct TappedProfilesState: Codable, Equatable {
-            var profiles: [UUID: TappedProfile] = [:]
+            var profiles: [UUID: TappedProfile] = .init()
+            
+            struct Modification {
+                var hi: String = ""
+            }
         }
         
         struct TabState: Equatable {
@@ -27,7 +31,46 @@ extension Redux {
 }
 
 extension Redux.State {
+    // sourcery: skipPropertyGeneration
     enum CodingKeys: String, CodingKey {
         case tappedProfiles
+    }
+}
+
+// sourcery: extension = "Redux"
+struct StateTree: Codable, Equatable, AutoTree {
+    struct TappedProfileState: Codable, Equatable {
+        var profiles: [UUID: TappedProfile]
+
+        struct Properties: Codable, Equatable {
+            var count: Int
+            var favourites: [UUID]
+        }
+    }
+
+    struct TabState: Equatable {
+        var currentTab: Tab
+    }
+}
+
+// sourcery: extension = "Redux"
+enum Action {
+    enum Refined: AutoTree {
+        enum TabAction {
+            case setTab(to: Tab)
+        }
+
+        enum TappedProfilesAction {
+            case add(TappedProfile)
+            case remove(UUID)
+            case removeMultiple(Set<UUID>)
+        }
+
+        case none
+    }
+
+    enum Raw {
+        case loadState
+        case saveState
     }
 }
