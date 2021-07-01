30d40596337421aa390f1fca0f685a5201528bb7
Nikita Mounier <nikita.mounier@gmail.com>
2021-07-02 00:37:34 +0200

    Add mocks to UUID and Date.

---
+++ Sources/GeneralMocks/Date.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+public extension Date {
+    static var now: Date {
+        return Date()
+    }
+    
+    static let oneHourAgo = Date(timeIntervalSinceNow: -3600)
+    static let oneDayAgo = Date(timeIntervalSinceNow: -86400)
+    static let oneWeekAgo = Date(timeIntervalSinceNow: -604800)
+    static let oneMonthAgo = Date(timeIntervalSinceNow: -2629746)
+}
---
+++ Sources/GeneralMocks/UUID.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+public extension UUID {
+    static let deadbeef = Self(uuidString: "deadbeef-dead-beef-dead-beefdeadbeef")!
+    
+    static let major1 = Self(uuidString: "deadbeef-dead-beef-dead-6d616a6f7231")!
+    static let minor1 = Self(uuidString: "deadbeef-dead-beef-dead-6d696e6f7231")!
+    
+    static let major2 = Self(uuidString: "deadbeef-dead-beef-dead-6d616a6f7232")!
+    static let minor2 = Self(uuidString: "deadbeef-dead-beef-dead-6d616a6f7231")!
+    
+    static let major3 = Self(uuidString: "deadbeef-dead-beef-dead-6d616a6f7233")!
+    static let minor3 = Self(uuidString: "deadbeef-dead-beef-dead-6d696e6f7233")!
+    
+}
