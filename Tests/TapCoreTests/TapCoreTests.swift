3f35631166c1f9d3a4f84519e810156717a8048c
Nikita Mounier <nikita.mounier@gmail.com>
2021-09-28 23:37:42 +0100

    Continue creating test for TapCore, with initial logic and some of the logic for ranging beacons.

--- Tests/TapCoreTests/TapCoreTests.swift
+++ Tests/TapCoreTests/TapCoreTests.swift
@@ -10,6 +10,7 @@ import SharedModels
 import XCTest
 
 @testable import TapCore
+import Network
 
 class TapCoreTests: XCTestCase {
     func testPerfectPath() {
@@ -87,7 +88,7 @@ class TapCoreTests: XCTestCase {
                 create: { _, _, _ in p2pListenerEventPublisher.eraseToEffect() },
                 startListening: { _ in .none },
                 stopListening: { _ in .none },
-                uuid: { .deadbeef }
+                uuid: { .deadbeef1 }
             ),
             connection: ConnectionClient(
                 create: { _, _ in p2pConnectionEventPublisher.eraseToEffect() },
@@ -107,8 +108,77 @@ class TapCoreTests: XCTestCase {
             stop: .none
         )
         
-        // MARK: - Initial logic
+        // MARK: - Initialization
+        
+        store.send(.startReconnectionTimer)
+        
+        store.send(.startBeacons)
+        
+        beaconDetectorEventPublisher.send(.authorizationChanged(.authorizedAlways))
+        scheduler.advance()
+        
+        beaconAdvertiserEventPublisher.send(.stateUpdated(.poweredOn))
+        scheduler.advance()
+        
+        store.send(.startP2P)
+        
+        p2pListenerEventPublisher.send(.stateUpdated(.ready))
+        scheduler.advance()
+        
+        p2pBrowserEventPublisher.send(.stateUpdated(.ready))
+        scheduler.advance()
         
         
+        // MARK: - Finding some beacon results
+        
+        scheduler.advance(by: 1)
+        
+        beaconDetectorEventPublisher.send(.ranged(
+            beacons: [
+                .init(uuid: .deadbeef, major: 120, minor: 430, proximity: .far, accuracy: 50, rssi: 20, timestamp: Date())
+            ]
+        ))
+        
+        store.receive(.rangedBeaconsResponse(
+            [.init(uuid: .deadbeef, major: 120, minor: 430, proximity: .far, accuracy: 50, rssi: 20, timestamp: Date())]
+        )) // shouldn't do anything since proxmity is .far
+        scheduler.advance()
+        
+        scheduler.advance(by: 1)
+        
+        beaconDetectorEventPublisher.send(.ranged(
+            beacons: [
+                .init(uuid: .deadbeef, major: 120, minor: 430, proximity: .far, accuracy: 50, rssi: 20, timestamp: Date()),
+                .init(uuid: .deadbeef1, major: 132, minor: 342, proximity: .near, accuracy: 30, rssi: 50, timestamp: Date()),
+                .init(uuid: .deadbeef2, major: 145, minor: 256, proximity: .near, accuracy: 15, rssi: 60, timestamp: Date()),
+            ]
+        ))
+        
+        scheduler.advance()
+        
+        store.receive(.rangedBeaconsResponse([
+            .init(uuid: .deadbeef, major: 120, minor: 430, proximity: .far, accuracy: 50, rssi: 20, timestamp: Date()),
+            .init(uuid: .deadbeef1, major: 132, minor: 342, proximity: .near, accuracy: 30, rssi: 50, timestamp: Date()),
+            .init(uuid: .deadbeef2, major: 145, minor: 256, proximity: .near, accuracy: 15, rssi: 60, timestamp: Date()),
+        ])) {
+            $0.peerInfo = "145-256" // takes last one since its accuracy value is lower (meaning it's closer)
+        }
+        
+        scheduler.advance(by: 0.5)
+        
+        // MARK: - Finding some P2P results
+        
+        p2pBrowserEventPublisher.send(.browseResultsChanged([
+            .added(BrowserResult(endpoint: .service(name: UUID.deadbeef.uuidString, type: "_deadbeef.tcp", domain: "", interface: nil), interfaces: [], metadata: .none))
+        ]))
+        scheduler.advance()
+        
+        store.receive(.browserResultsChangedResponse([
+            .added(BrowserResult(endpoint: .service(name: UUID.deadbeef.uuidString, type: "_deadbeef.tcp", domain: "", interface: nil), interfaces: [], metadata: .none))
+        ])) {
+            $0.browserResults.insert(BrowserResult(endpoint: .service(name: UUID.deadbeef.uuidString, type: "_deadbeef.tcp", domain: "", interface: nil), interfaces: [], metadata: .none))
+        }
+        
+        store.receive(.attemptNewConnections)
     }
 }
