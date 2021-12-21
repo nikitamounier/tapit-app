139d316bbb04c9f672645aebb6bb412734165182
Nikita Mounier <nikita.mounier@gmail.com>
2021-12-21 22:19:14 +0000

    Create UserProfileFeaturePreview, a mini-app for the (currently barebones) HistoryFeature.

---
+++ App/Previews/UserProfileFeaturePreview/UserProfileFeaturePreviewApp.swift
@@ -0,0 +1,17 @@
+import UserProfileFeature
+import SwiftUI
+
+@main
+struct UserProfileFeaturePreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            UserProfileView(
+                store: .init(
+                    initialState: .init(profile: .mock),
+                    reducer: userProfileReducer,
+                    environment: UserProfileEnvironment.init()
+                )
+            )
+        }
+    }
+}
