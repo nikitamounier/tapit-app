fe92bc6f346a54e6a4beedb83f2642462061d5ac
Nikita Mounier <nikita.mounier@gmail.com>
2021-12-21 22:17:58 +0000

    Create HistoryPreviewApp, a mini-app for the HistoryFeature.

---
+++ App/Previews/HistoryFeaturePreview/HistoryFeaturePreviewApp.swift
@@ -0,0 +1,23 @@
+import HistoryFeature
+import SwiftUI
+
+@main
+struct HistoryFeaturePreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HistoryView(
+                store: .init(
+                    initialState: .init(profiles: Array(repeating: .mock, count: 20)),
+                    reducer: historyReducer,
+                    environment: .init(
+                        mainQueue: .main,
+                        feedbackGenerator: .noop,
+                        isSentProfileExpired: .noop,
+                        openSocial: .noop,
+                        openAppSettings: {}
+                    )
+                )
+            )
+        }
+    }
+}
