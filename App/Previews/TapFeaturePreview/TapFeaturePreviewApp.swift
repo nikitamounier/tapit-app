de4fb094ec3b26d6e9cc1002bd5a57d98b4bbe9c
Nikita Mounier <nikita.mounier@gmail.com>
2021-12-23 19:10:58 +0000

    Remove .mockFacebook() from TapFeaturePreview mock data because of hash collision.

--- App/Previews/TapFeaturePreview/TapFeaturePreviewApp.swift
+++ App/Previews/TapFeaturePreview/TapFeaturePreviewApp.swift
@@ -5,33 +5,31 @@ import TapFeature
 struct TapFeaturePreviewApp: App {
     var body: some Scene {
         WindowGroup {
-            ScrollView {
-                TapFeatureView(
-                    store: .init(
-                        initialState: .init(
-                            profile: .init(
-                                id: .deadbeef,
-                                name: "Bob",
-                                profileImage: .mock,
-                                socials: [.mockInstagram(), .mockSnapchat(), .mockTwitter(), .mockFacebook(), .mockReddit(), .mockTikTok(), .mockWeChat(), .mockGithub(), .mockLinkedIn(), .mockAddress(), .mockEmail(), .mockPhone()]
-                                          )
-                        ),
-                        reducer: tapFeatureReducer,
-                        environment: .init(
-                            mainQueue: .main,
-                            beaconQueue: .immediate,
-                            beacon: .noop,
-                            p2p: .noop,
-                            p2pEncodeDecode: .noop,
-                            feedbackGenerator: .noop,
-                            proximitySensor: .noop,
-                            orientation: .noop,
-                            dispatchNow: { .now() },
-                            openAppSettings: {}
+            TapFeatureView(
+                store: .init(
+                    initialState: .init(
+                        profile: .init(
+                            id: .deadbeef,
+                            name: "Bob",
+                            profileImage: .mock,
+                            socials: [.mockInstagram(), .mockSnapchat(), .mockTwitter(), .mockReddit(), .mockTikTok(), .mockWeChat(), .mockGithub(), .mockLinkedIn(), .mockAddress(), .mockEmail(), .mockPhone()]
                         )
+                    ),
+                    reducer: tapFeatureReducer,
+                    environment: .init(
+                        mainQueue: .main,
+                        beaconQueue: .immediate,
+                        beacon: .noop,
+                        p2p: .noop,
+                        p2pEncodeDecode: .noop,
+                        feedbackGenerator: .noop,
+                        proximitySensor: .noop,
+                        orientation: .noop,
+                        dispatchNow: { .now() },
+                        openAppSettings: {}
                     )
                 )
-            }
+            )
         }
     }
 }
