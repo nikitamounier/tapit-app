28cac2befc5104aced6d3bc8f70cf6f8eb732adb
Nikita Mounier <nikita.mounier@gmail.com>
2021-12-24 01:03:41 +0000

    Add some more initial state in TapFeaturePreview.

--- App/Previews/TapFeaturePreview/TapFeaturePreviewApp.swift
+++ App/Previews/TapFeaturePreview/TapFeaturePreviewApp.swift
@@ -1,35 +1,45 @@
+import SharedModels
 import SwiftUI
 import TapFeature
+
 
 @main
 struct TapFeaturePreviewApp: App {
     var body: some Scene {
         WindowGroup {
-            TapFeatureView(
-                store: .init(
-                    initialState: .init(
-                        profile: .init(
-                            id: .deadbeef,
-                            name: "Bob",
-                            profileImage: .mock,
-                            socials: [.mockInstagram(), .mockSnapchat(), .mockTwitter(), .mockReddit(), .mockTikTok(), .mockWeChat(), .mockGithub(), .mockLinkedIn(), .mockAddress(), .mockEmail(), .mockPhone()]
+            NavigationView {
+                TapFeatureView(
+                    store: .init(
+                        initialState: .init(
+                            profile: .init(
+                                id: .deadbeef,
+                                name: "Bob",
+                                profileImage: .mock,
+                                socials: [.mockInstagram(), .mockSnapchat(), .mockTwitter(), .mockReddit(), .mockTikTok(), .mockWeChat(), .mockGithub(), .mockLinkedIn(), .mockAddress(), .mockEmail(), .mockPhone()]
+                            ),
+                            presets: [
+                                .init(name: "Friend", socials: [Social.mockInstagram().id, Social.mockTikTok().id]),
+                                .init(name: "Potential?", socials: [Social.mockInstagram().id, Social.mockSnapchat().id, Social.mockPhone().id])
+                            ],
+                            selectedSocials: []
+                        ),
+                        reducer: tapFeatureReducer,
+                        environment: .init(
+                            mainQueue: .main,
+                            beaconQueue: .immediate,
+                            beacon: .noop,
+                            p2p: .noop,
+                            p2pEncodeDecode: .noop,
+                            feedbackGenerator: .noop,
+                            proximitySensor: .noop,
+                            orientation: .noop,
+                            dispatchNow: { .now() },
+                            openAppSettings: {}
                         )
-                    ),
-                    reducer: tapFeatureReducer,
-                    environment: .init(
-                        mainQueue: .main,
-                        beaconQueue: .immediate,
-                        beacon: .noop,
-                        p2p: .noop,
-                        p2pEncodeDecode: .noop,
-                        feedbackGenerator: .noop,
-                        proximitySensor: .noop,
-                        orientation: .noop,
-                        dispatchNow: { .now() },
-                        openAppSettings: {}
                     )
                 )
-            )
+                .navigationTitle(Text("Tap It"))
+            }
         }
     }
 }
