7bf8a99ff3a3ef36f4c985ad542e7797d6af0250
Nikita Mounier <nikita.mounier@gmail.com>
2021-12-29 15:44:01 -0500

    Factor out the bottom sheet's animation.

--- Sources/BottomMenu/BottomMenu.swift
+++ Sources/BottomMenu/BottomMenu.swift
@@ -7,9 +7,10 @@ public extension View {
         @ViewBuilder header: @escaping () -> HeaderContent,
         scrollingMain: () -> ScrollView<MainContent>
     ) -> some View {
+        
         self.bottomSheet(
             bottomSheetPosition: sheetPosition,
-            options: [.noBottomPosition, .appleScrollBehavior, .backgroundBlur(effect: .systemThinMaterial), .tapToDissmiss, .swipeToDismiss],
+            options: [.noBottomPosition, .appleScrollBehavior, .backgroundBlur(effect: .systemThinMaterial), .tapToDissmiss, .swipeToDismiss, .animation(SheetPosition.animation)],
             headerContent: header,
             mainContent: { scrollingMain().content }
         )
@@ -22,7 +23,7 @@ public extension View {
     ) -> some View {
         self.bottomSheet(
             bottomSheetPosition: sheetPosition,
-            options: [.noBottomPosition, .allowContentDrag, .backgroundBlur(effect: .systemThinMaterial), .tapToDissmiss, .swipeToDismiss],
+            options: [.noBottomPosition, .allowContentDrag, .backgroundBlur(effect: .systemThinMaterial), .tapToDissmiss, .swipeToDismiss, .animation(SheetPosition.animation)],
             headerContent: header,
             mainContent: main
         )
@@ -33,24 +34,33 @@ public enum SheetPosition: CGFloat, CaseIterable {
     case top = 0.975
     case middle = 0.4
     case hidden = 0
+    
+    static let animation: Animation = .spring(response: 0.3, dampingFraction: 0.75, blendDuration: 1)
 }
 
 struct BottomMenu_Previews: PreviewProvider {
     struct Preview: View {
         @State private var sheetPosition: SheetPosition = .hidden
+        @State private var showSheet = false
         
         var body: some View {
-            Color.blue
-                .onTapGesture {
-                    withAnimation(.spring(response: 0.5, dampingFraction: 0.75, blendDuration: 1)) {
-                        sheetPosition = .middle
+            VStack {
+                Color.blue
+                    .onTapGesture {
+                        withAnimation(SheetPosition.animation) {
+                            sheetPosition = .middle
+                        }
                     }
-                }
-                .bottomMenu(sheetPosition: $sheetPosition, header: {
-                    Text("Hello there")
-                }, scrollingMain: {
-                    ScrollView {
-                        LazyVStack {
+                Color.red
+                    .onTapGesture {
+                        showSheet = true
+                    }
+            }
+            .bottomMenu(sheetPosition: $sheetPosition, header: {
+                Text("Hello there")
+            }, scrollingMain: {
+                ScrollView {
+                    LazyVStack {
                         ForEach(0..<100) { n in
                             Label("The number is \(n)", systemImage: "circle")
                                 .font(.title2)
@@ -58,9 +68,9 @@ struct BottomMenu_Previews: PreviewProvider {
                                 .frame(maxWidth: .infinity, alignment: .leading)
                             Divider().padding(.leading)
                         }
-                        }
                     }
-                })
+                }
+            })
         }
     }
     
