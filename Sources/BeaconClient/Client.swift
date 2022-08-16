d240ddc28508822b5c39cf74a67d0d1d839d7441
Nikita Mounier <nikita.mounier@gmail.com>
2022-08-16 11:06:37 -0400

    Add global actor with custom RunLoop for BeaconClient.

--- Sources/BeaconClient/Client.swift
+++ Sources/BeaconClient/Client.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public struct BeaconClient {
-  public var start: @BeaconActor @Sendable (_ major: UInt16, _ minor: UInt16) async -> AsyncThrowingStream<[Beacon], Error>
+  public var start: @Sendable (_ major: UInt16, _ minor: UInt16) async -> AsyncThrowingStream<[Beacon], Error>
 
   public init(start: @escaping @Sendable (_ major: UInt16, _ minor: UInt16) async -> AsyncThrowingStream<[Beacon], Error>) {
     self.start = start
@@ -17,71 +17,5 @@ public struct Beacon: Equatable {
   
   public enum Proximity: Int {
     case unknown, immediate, near, far
-  }
-}
-
-@globalActor
-actor BeaconActor {
-  static let shared = BeaconActor()
-  
-  init() {
-    let executor = RunLoopExecutor()
-    self.executor = executor
-    unownedExecutor = executor.asUnownedSerialExecutor()
-    executor.start()
-  }
-  
-  nonisolated let executor: SerialExecutor
-  nonisolated let unownedExecutor: UnownedSerialExecutor
-  
-  
-  private final class RunLoopExecutor: Thread, SerialExecutor, @unchecked Sendable {
-    override init() {
-      super.init()
-      name = "AccessibilityElement.SystemObserver"
-      qualityOfService = .userInitiated
-    }
-    override func main() {
-      autoreleasepool {
-        // Toss something on the run loop so it doesn't return right away
-        Timer.scheduledTimer(timeInterval: Date.distantFuture.timeIntervalSince1970,
-                             target: self,
-                             selector: #selector(noop),
-                             userInfo: nil,
-                             repeats: true)
-        while true {
-          autoreleasepool {
-            _ = RunLoop.current.run(mode: .default, before: Date(timeIntervalSinceNow: 1.0))
-          }
-        }
-      }
-    }
-    @objc func noop() {}
-    // Stick UnownedJob inside a reference type
-    private class Job: NSObject {
-      private let unownedJob: UnownedJob
-      init(unownedJob: UnownedJob) {
-        self.unownedJob = unownedJob
-      }
-      func runSynchronously(on unownedExecutor: UnownedSerialExecutor) {
-        unownedJob._runSynchronously(on: unownedExecutor)
-      }
-    }
-    
-    func enqueue(_ job: UnownedJob) {
-      perform(#selector(enqueueOnRunLoop),
-              on: self,
-              with: Job(unownedJob: job),
-              waitUntilDone: false,
-              modes: [RunLoop.Mode.default.rawValue])
-    }
-    
-    func asUnownedSerialExecutor() -> UnownedSerialExecutor {
-      UnownedSerialExecutor(ordinary: self)
-    }
-    
-    @objc private func enqueueOnRunLoop(_ job: Job) {
-      job.runSynchronously(on: asUnownedSerialExecutor())
-    }
   }
 }
--- Sources/BeaconClient/Live.swift
+++ Sources/BeaconClient/Live.swift
@@ -191,6 +191,73 @@ private final class Delegate: NSObject, CLLocationManagerDelegate, CBPeripheralM
   }
 }
 
+@globalActor
+actor BeaconActor {
+  static let shared = BeaconActor()
+  
+  init() {
+    let executor = RunLoopExecutor()
+    self.executor = executor
+    unownedExecutor = executor.asUnownedSerialExecutor()
+    executor.start()
+  }
+  
+  nonisolated let executor: SerialExecutor
+  nonisolated let unownedExecutor: UnownedSerialExecutor
+  
+  
+  private final class RunLoopExecutor: Thread, SerialExecutor, @unchecked Sendable {
+    override init() {
+      super.init()
+      name = "AccessibilityElement.SystemObserver"
+      qualityOfService = .userInitiated
+    }
+    override func main() {
+      autoreleasepool {
+        // Toss something on the run loop so it doesn't return right away
+        Timer.scheduledTimer(timeInterval: Date.distantFuture.timeIntervalSince1970,
+                             target: self,
+                             selector: #selector(noop),
+                             userInfo: nil,
+                             repeats: true)
+        while true {
+          autoreleasepool {
+            _ = RunLoop.current.run(mode: .default, before: Date(timeIntervalSinceNow: 1.0))
+          }
+        }
+      }
+    }
+    @objc func noop() {}
+    // Stick UnownedJob inside a reference type
+    private class Job: NSObject {
+      private let unownedJob: UnownedJob
+      init(unownedJob: UnownedJob) {
+        self.unownedJob = unownedJob
+      }
+      func runSynchronously(on unownedExecutor: UnownedSerialExecutor) {
+        unownedJob._runSynchronously(on: unownedExecutor)
+      }
+    }
+    
+    func enqueue(_ job: UnownedJob) {
+      perform(#selector(enqueueOnRunLoop),
+              on: self,
+              with: Job(unownedJob: job),
+              waitUntilDone: false,
+              modes: [RunLoop.Mode.default.rawValue])
+    }
+    
+    func asUnownedSerialExecutor() -> UnownedSerialExecutor {
+      UnownedSerialExecutor(ordinary: self)
+    }
+    
+    @objc private func enqueueOnRunLoop(_ job: Job) {
+      job.runSynchronously(on: asUnownedSerialExecutor())
+    }
+  }
+}
+
+
 @propertyWrapper
 private final class Box<Value> {
   var wrappedValue: Value
