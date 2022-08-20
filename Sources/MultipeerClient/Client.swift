b642d0244b1850345b580f03694cc4dfc7405041
Nikita Mounier <nikita.mounier@gmail.com>
2022-08-20 16:04:07 -0400

    Make receiving profile a throwing function, in case it's received from the wrong person.

--- Sources/MultipeerClient/Client.swift
+++ Sources/MultipeerClient/Client.swift
@@ -4,12 +4,12 @@ import SharedModels
 public struct MultipeerClient {
   public var start: @Sendable (_ peerID: String) async -> AsyncStream<PeerID>
   public var send: @Sendable (UserProfile, _ to: PeerID) async throws -> ()
-  public var receive: @Sendable (_ from: PeerID) async -> UserProfile
+  public var receive: @Sendable (_ from: PeerID) async throws -> UserProfile
   
   public init(
     start: @escaping @Sendable (_ peerID: String) async -> AsyncStream<PeerID>,
     send: @escaping @Sendable (UserProfile, _ to: PeerID) async throws -> (),
-    receive: @escaping @Sendable (_ from: PeerID) async -> UserProfile
+    receive: @escaping @Sendable (_ from: PeerID) async throws -> UserProfile
   ) {
     self.start = start
     self.send = send
@@ -26,3 +26,4 @@ public struct PeerID: Equatable {
   }
 }
 
+public enum CancelMultipeerID {}
--- Sources/MultipeerClient/Live.swift
+++ Sources/MultipeerClient/Live.swift
@@ -4,64 +4,67 @@ import ComposableArchitecture
 
 
 public extension MultipeerClient {
-    static var live: Self {
-        let multipeer = Multipeer()
-        
-        return Self(
-            start: { await multipeer.start(peerID: $0) } ,
-            send: { try await multipeer.send(profile: $0, to: $1) },
-            receive: { await multipeer.receive(from: $0) }
-        )
-    }
+  static var live: Self {
+    let multipeer = Multipeer()
+    
+    return Self(
+      start: { await multipeer.start(peerID: $0) } ,
+      send: { try await multipeer.send(profile: $0, to: $1) },
+      receive: { try await multipeer.receive(from: $0) }
+    )
+  }
 }
 
 private actor Multipeer {
-    var transceiver: MultipeerTransceiver?
+  var transceiver: MultipeerTransceiver?
+  
+  enum Error: Swift.Error, Equatable {
+    case peerUnavailable
+  }
+  
+  func start(peerID: String) -> AsyncStream<PeerID> {
     
-    enum Error: Swift.Error, Equatable {
-        case peerUnavailable
+    var configuration: MultipeerConfiguration = .default
+    configuration.peerName = peerID
+    configuration.serviceType = "tapit"
+    // TODO: - Change encryption to .required once have Apple Developer Program Membership
+    configuration.security.encryptionPreference = .none
+    
+    guard !Task.isCancelled else { return .finished }
+    
+    transceiver = MultipeerTransceiver(configuration: configuration)
+    transceiver?.resume()
+    
+    return AsyncStream { continuation in
+      transceiver?.peerAdded = { newPeer in
+        continuation.yield(PeerID(name: newPeer.name))
+      }
+      
+      continuation.onTermination = { @Sendable [transceiver = UncheckedSendable(transceiver)] _ in
+        transceiver.wrappedValue?.stop()
+      }
     }
+  }
+  
+  func send(profile: UserProfile, to peerID: PeerID) throws {
+    guard let peer = transceiver!.availablePeers.first(where: { $0.name == peerID.name })
+    else { throw Error.peerUnavailable }
     
-    func start(peerID: String) -> AsyncStream<PeerID> {
-        
-        var configuration: MultipeerConfiguration = .default
-        configuration.peerName = peerID
-        configuration.serviceType = "tapit"
-        // TODO: - Change encryption to .required once have Apple Developer Program Membership
-        configuration.security.encryptionPreference = .none
-        
-        transceiver = MultipeerTransceiver(configuration: configuration)
-        transceiver?.resume()
-        
-        return AsyncStream { continuation in
-            transceiver?.peerAdded = { newPeer in
-                continuation.yield(PeerID(name: newPeer.name))
-            }
-            
-            continuation.onTermination = { @Sendable [transceiver = UncheckedSendable(transceiver)] _ in
-                transceiver.wrappedValue?.stop()
-            }
+    transceiver?.send(profile, to: [peer])
+  }
+  
+  func receive(from peerID: PeerID) async throws -> UserProfile {
+    try Task.checkCancellation()
+    return try await withCheckedThrowingContinuation { continuation in
+        transceiver?.receive(UserProfile.self) { payload, senderPeer in
+          guard senderPeer.name == peerID.name
+          else {
+            continuation.resume(throwing: Error.peerUnavailable)
+            return
+          }
+          
+          continuation.resume(returning: payload)
         }
     }
-    
-    func send(profile: UserProfile, to peerID: PeerID) throws {
-        guard let peer = transceiver!.availablePeers.first(where: { $0.name == peerID.name })
-        else { throw Error.peerUnavailable }
-        
-        transceiver?.send(profile, to: [peer])
-    }
-    
-    func receive(from peerID: PeerID) async -> UserProfile {
-        await withCheckedContinuation { continuation in
-            transceiver?.receive(UserProfile.self) { payload, senderPeer in
-                guard senderPeer.name == peerID.name
-                else {
-                  print("Received profile from wrong peer")
-                  return
-                }
-                
-                continuation.resume(returning: payload)
-            }
-        }
-    }
+  }
 }
